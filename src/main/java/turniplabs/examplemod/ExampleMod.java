11b5f4a5643c6188b17531a7a349085f994bef47
azurelmao <de_arcane@protonmail.com>
2023-01-19 20:09:46 +0100

    Update dependencies

--- src/main/java/turniplabs/examplemod/ExampleMod.java
+++ src/main/java/turniplabs/examplemod/ExampleMod.java
@@ -9,10 +9,6 @@ public class ExampleMod implements ModInitializer {
     public static final String MOD_ID = "examplemod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static String name(String name) {
-        return ExampleMod.MOD_ID + "." + name;
-    }
-
     @Override
     public void onInitialize() {
         LOGGER.info("ExampleMod initialized.");
