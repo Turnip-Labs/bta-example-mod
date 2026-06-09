42e77dd5fdf88274ec147921f2d3180074a3cbc4
bigsir24 <169484568+bigsir24@users.noreply.github.com>
2026-06-09 15:48:51 +0200

    Move natives resolution to separate function

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,33 +3,7 @@ plugins {
     java
 }
 
-val lwjglNatives = Pair(
-	System.getProperty("os.name")!!,
-	System.getProperty("os.arch")!!
-).let { (name, arch) ->
-	when {
-		"FreeBSD" == name ->
-			"natives-freebsd"
-		arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
-			if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
-				"natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
-			else if (arch.startsWith("ppc"))
-				"natives-linux-ppc64le"
-			else if (arch.startsWith("riscv"))
-				"natives-linux-riscv64"
-			else
-				"natives-linux"
-		arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } ->
-			"natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
-		arrayOf("Windows").any { name.startsWith(it) } ->
-			if (arch.contains("64"))
-				"natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
-			else
-				"natives-windows-x86"
-		else ->
-			throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
-	}
-}
+val lwjglNatives = resolveLwjglNatives()
 
 val modVersion = "${providers.gradleProperty("mod_version").get()}+${libs.versions.bta.get()}"
 val modGroup: Provider<String> = providers.gradleProperty("mod_group")
@@ -158,4 +132,34 @@ configurations.configureEach {
 	exclude(group = "net.java.jinput")
 	exclude(group = "net.sf.jopt-simple")
 	exclude(group = "net.minecraft", module = "launchwrapper")
+}
+
+fun resolveLwjglNatives(): String { // Sourced from https://www.lwjgl.org/
+	return Pair(
+		System.getProperty("os.name")!!,
+		System.getProperty("os.arch")!!
+	).let { (name, arch) ->
+		when {
+			"FreeBSD" == name ->
+				"natives-freebsd"
+			arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
+				if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+					"natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+				else if (arch.startsWith("ppc"))
+					"natives-linux-ppc64le"
+				else if (arch.startsWith("riscv"))
+					"natives-linux-riscv64"
+				else
+					"natives-linux"
+			arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } ->
+				"natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+			arrayOf("Windows").any { name.startsWith(it) } ->
+				if (arch.contains("64"))
+					"natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+				else
+					"natives-windows-x86"
+			else ->
+				throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+		}
+	}
 }
