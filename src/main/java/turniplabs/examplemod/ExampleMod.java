c97797e111202f8c7d23c0e661260200a8019ffa
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2026-07-12 15:13:14 +0200

    Update to use new halplibe event system.

--- src/main/java/turniplabs/examplemod/ExampleMod.java
+++ src/main/java/turniplabs/examplemod/ExampleMod.java
@@ -4,35 +4,25 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.HalpLibe;
-import turniplabs.halplibe.util.GameStartEntrypoint;
-import turniplabs.halplibe.util.RecipeEntrypoint;
+import turniplabs.halplibe.event.defs.CommonEvents;
+import turniplabs.halplibe.util.dependency.Key;
 
-public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = HalpLibe.registerMod("examplemod", true);
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		CommonEvents.BEFORE_GAME_START.listen(Key.of(MOD_ID), this::beforeGameStart);
+		CommonEvents.AFTER_GAME_START.listen(Key.of(MOD_ID),this::afterGameStart);
 		LOGGER.info("ExampleMod initialized.");
 	}
 
-	@Override
 	public void beforeGameStart() {
 
 	}
 
-	@Override
 	public void afterGameStart() {
-
-	}
-
-	@Override
-	public void onRecipesReady() {
-
-	}
-
-	@Override
-	public void initNamespaces() {
 
 	}
 }
